7a1f16314f08617d586229a61f5f01fbe227989c
Abhishekdagar <abhishek.dagar@nagarro.com>
2025-08-11 09:12:15 +0000

    reverted back

--- JustEat/JustEat/api/index.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-# Handler for serverless function
-def handler(request):
-    return app(request)
--- JustEat/JustEat/app.py
+++ JustEat/JustEat/app.py
@@ -2,7 +2,6 @@ from app import create_app
 
 app = create_app()
 
-# Vercel needs to import 'app' directly
-# Keep the debug run for local development
+
 if __name__ == "__main__":
     app.run(debug=True)

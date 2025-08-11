f635efddf1b665f1f92b9586f00830e6dbab8378
Abhishekdagar <abhishek.dagar@nagarro.com>
2025-08-11 08:59:41 +0000

    updates in app and vercel config

--- JustEat/JustEat/app.py
+++ JustEat/JustEat/app.py
@@ -2,6 +2,7 @@ from app import create_app
 
 app = create_app()
 
-
+# Vercel needs to import 'app' directly
+# Keep the debug run for local development
 if __name__ == "__main__":
     app.run(debug=True)
